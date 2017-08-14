ecf399388a491f5bdefba79c26e32bb7297fafd9
Cassidy Hales <cassidyhales11@gmail.com>
2017-08-13 22:37:18 -0600

    Finished week three rails assignment

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20170814043632) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "content"
+    t.string   "tags"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "projects", force: :cascade do |t|
+    t.string   "title"
+    t.text     "content"
+    t.text     "tags"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
